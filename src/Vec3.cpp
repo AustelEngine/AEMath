9563e9ad7cccf3eefcd9f9513821a7073071b5c3
Anthony <32407457+anthonyholstein@users.noreply.github.com>
2021-01-02 11:47:47 -0500

    Updated the copyright

--- src/Vec3.cpp
+++ src/Vec3.cpp
@@ -1,9 +1,7 @@
-//     AustelEngine(C) 2019 - Nietsloh
-//----------------------------------------
-//		Author      : Anthony Holstein
-//		File        : Vec3.cpp
-//		Description : todo
-//----------------------------------------
+/*
+ *
+ * © 2019-2021 AustelEngine, a project from Nietsloh. All rights reserved.
+ */
 
 #include "Vec3.h"
 
