cee5c7085a7891a320079b0e4dfbaa62e3b2ab98
Anthony <32407457+anthonyholstein@users.noreply.github.com>
2021-01-02 11:46:48 -0500

    Updated the copyright

--- src/Math.h
+++ src/Math.h
@@ -1,10 +1,7 @@
-//     AustelEngine(C) 2019 - Nietsloh
-//----------------------------------------
-//		Author      : Anthony Holstein
-//		File        : Math.h
-//		Description : todo
-//----------------------------------------
-
+/*
+ *
+ * © 2019-2021 AustelEngine, a project from Nietsloh. All rights reserved.
+ */
 
 #pragma once
 
